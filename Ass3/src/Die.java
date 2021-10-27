7f149be62807355b677da78a7e5c515ac6e9815e
Aryamann Mishra <88878018+Luciferr910@users.noreply.github.com>
2021-10-27 23:20:15 +0530

    Add files via upload

---
+++ Ass3/src/Die.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Die {
+
+    private Random random;
+
+    public Die(){
+        random = new Random();
+    }
+    /**
+     * Rolls a D6 and returns the value.
+     * @return An random int value between 1-6 (inclusive)
+     */
+    public int rollD6(){
+        return random.nextInt(6)+1;
+    }
+}
